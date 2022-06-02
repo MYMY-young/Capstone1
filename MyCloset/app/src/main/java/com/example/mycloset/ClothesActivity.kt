67bd41fc03efac7c91836805376c6ff7d011ad37
younseo-leee <77726052+younseo-leee@users.noreply.github.com>
2022-06-02 15:52:28 +0900

    highlight activity 추가

--- MyCloset/app/src/main/java/com/example/mycloset/ClothesActivity.kt
+++ MyCloset/app/src/main/java/com/example/mycloset/ClothesActivity.kt
@@ -1,18 +1,26 @@
 package com.example.mycloset
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 
 class ClothesActivity : AppCompatActivity() {
     var _backButton: ImageView? = null
+    var _nextButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clothes)
 
         _backButton = findViewById(R.id.clothes_back_button) as ImageView
-        _backButton!!.setOnClickListener { finish() }
+        _nextButton = findViewById(R.id.clothes_next_button) as Button
 
+        _backButton!!.setOnClickListener { finish() }
+        _nextButton!!.setOnClickListener {
+            startActivity(Intent(this, HighlightActivity::class.java))
+            _nextButton!!.isEnabled = true
+        }
     }
 }
